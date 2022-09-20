e00b9abaf1f36d14c2dfd8a2dc2741506306629d
ragib70 <ragib.hussain70@gmail.com>
2022-09-20 17:57:30 +0530

    NavBar Added

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Main from "./components/Main/main"
+
+function App() {
+  return <div className="App">
+    <Main/>
+  </div>;
+}
+
+export default App;
---
+++ src/components/Main/main.js
@@ -0,0 +1,12 @@
+import "./main.css";
+import ResponsiveAppBar from "../NavBar/responsiveappbar";
+
+function Main() {
+  return (
+    <div>
+      <ResponsiveAppBar />
+    </div>
+  );
+}
+
+export default Main;
